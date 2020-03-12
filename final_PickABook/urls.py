e703fff3c988cb7cf0834664bd033d70a9086172
Avinash Rao <avinash_it@yahoo.com>
2020-03-12 14:56:45 +0530

    profile page working fine.. now need to edit it to work for my project

--- final_PickABook/urls.py
+++ final_PickABook/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('',include('LandingApp.urls')),
+    url('profile',include('userprofile.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
