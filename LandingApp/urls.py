0b9cdb56d9a48119108a788137c1a4ed445cee61
Avinash Rao <avinash_it@yahoo.com>
2020-03-25 21:44:57 +0530

    Nothing

--- LandingApp/urls.py
+++ LandingApp/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 
 urlpatterns=[
     url('^$',views.firstpage, name='index'),
-    url('login',views.userLogin),
-    url('register',views.register),
-    url('contact',views.contact),
-    url('about',views.about),
-    url('Reset-password',views.ChangePassword),
-    url('Generate-password',views.GeneratePassword),
+    url('login',views.userLogin, name='login'),
+    url('register',views.register, name='register'),
+    url('contact',views.contact, name='contact'),
+    url('about',views.about, name='about'),
+    url('Reset-password',views.ChangePassword, name='changePassword'),
+    url('Generate-password',views.GeneratePassword, name='generatePassword'),
 ]
--- books/urls.py
+++ books/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 
 urlpatterns=[
     #localhost/books/
-    url('^$',views.bookss),
+    url('^$',views.bookss, name='books'),
     url('add-book', views.addBook),
 
     #http://127.0.0.1:8000/books/book-detail/1
     url('book-detail/(?P<book_id>\d+)', views.book_detail),
-    
+
 ]
