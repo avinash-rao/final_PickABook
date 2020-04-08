2409cec296bd14956605457b2b1d91a7d8c637e8
Avinash Rao <avinash_it@yahoo.com>
2020-04-08 11:56:08 +0530

    fixed z-index issue

--- final_PickABook/urls.py
+++ final_PickABook/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('LandingApp.urls')),
-    url('profile', include('userprofile.urls')),
-    url('books', include('books.urls')),
+    url('profile/', include('userprofile.urls')),
+    url('books/', include('books.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
