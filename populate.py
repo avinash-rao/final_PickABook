1f320c28fe090fe4d550e88b1d9566a7b2172b56
Avinash Rao <avinash_it@yahoo.com>
2020-04-15 17:21:27 +0530

    populated users

--- populate.py
+++ populate.py
@@ -1,6 +1,6 @@
 import os
 import django
-from books.models import *
+
 
 def populate():
     # genre_list = ['Fantasy', 'Adventure', 'Romance', 'Contemporary',
@@ -11,9 +11,32 @@ def populate():
     # for genre in genre_list:
     #     add_genre(genre)
 
-    books = Book.objects.all()
-    for book in books:
-        book.delete()
+    # books = Book.objects.all()
+    # for book in books:
+    #     book.delete()
+
+    # users = User.objects.all()
+    # for user in users:
+    #     user.delete()
+
+    users = [
+            {'mail':'rahulgeorge0009@gmail.com', 'first_name':'Rahul', 'last_name':'George'},
+            {'mail':'rajnipriya1210@gmail.com', 'first_name':'RajniPriya', 'last_name':''},
+            {'mail':'utpal38bpc@gmail.com', 'first_name':'Utpal', 'last_name':'Gaurav'},
+            {'mail':'qureshiyusuff@gmail.com', 'first_name':'Yousuf', 'last_name':'Qureshi'},
+            {'mail':'wahid786ali007@gmail.com', 'first_name':'Wahid', 'last_name':'Ali'},
+          ]
+    # for user in users:
+    #     addUser(user)
+
+    
+
+def addUser(obj):
+    u = User.objects.create_user(username=obj['mail'],
+                                    password='Test1234',
+                                    email=obj['mail'],
+                                    first_name=obj['first_name'],
+                                    last_name=obj['last_name'])
 
 def add_genre(name):
   g = Genre.objects.get_or_create(name=name)
@@ -24,4 +47,6 @@ if __name__ == '__main__':
   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_PickABook.settings')
   django.setup()
   from books.models import Genre
+  from books.models import *
+  from django.contrib.auth.models import User
   populate()
